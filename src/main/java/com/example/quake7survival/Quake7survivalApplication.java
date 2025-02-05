0847167c29a56e0a6aaed58e4c1eb31a9bd0dc59
levure-s <levure.sauvage@gmail.com>
2025-02-05 20:05:54 +0900

    first_commit

---
+++ src/main/java/com/example/quake7survival/Quake7survivalApplication.java
@@ -0,0 +1,13 @@
+package com.example.quake7survival;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Quake7survivalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Quake7survivalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/quake7survival/Quake7survivalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quake7survival;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Quake7survivalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
